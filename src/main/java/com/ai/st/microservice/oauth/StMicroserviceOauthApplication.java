254be4e05efa3888bad96a77f3a4b3b57a190968
Jhon Freddy Rondon <jhonfr29@gmail.com>
2019-11-07 13:46:43 -0500

    configuration dependencies project

---
+++ src/main/java/com/ai/st/microservice/oauth/StMicroserviceOauthApplication.java
@@ -0,0 +1,13 @@
+package com.ai.st.microservice.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StMicroserviceOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StMicroserviceOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/st/microservice/oauth/StMicroserviceOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.st.microservice.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StMicroserviceOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
